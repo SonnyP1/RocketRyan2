89a1c016b24e75cd039e89f4f7d8bd1fa6449bcc
Sonny Ponce <sonnyjponce@gmail.com>
2022-04-16 16:03:31 -0500

    Added level looping

--- Assets/_Arcade/Framework/ScoreKeeper.cs
+++ Assets/_Arcade/Framework/ScoreKeeper.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreKeeper : MonoBehaviour
 {
     List<GameObject> AllEnemies = new List<GameObject>();
     GameplayUIManager _gameplayUIManager;
+    int levelIndex = 1;
     int score = 0;
 
     private void Start()
     {
+        DontDestroyOnLoad(gameObject);
         _gameplayUIManager = FindObjectOfType<GameplayUIManager>();
+        if(_gameplayUIManager == null)
+        {
+            return;
+        }
         _gameplayUIManager.UpdateScoreCountTxt(score);
     }
     public void AddToEnemyList(GameObject newEnemy)
@@ -32,5 +39,15 @@ public class ScoreKeeper : MonoBehaviour
         }
         _gameplayUIManager.UpdateEnemyCountTxt(AllEnemies.Count);
         _gameplayUIManager.UpdateScoreCountTxt(score);
+    }
+
+    private void Update()
+    {
+        if(AllEnemies.Count == 0 && SceneManager.GetActiveScene().buildIndex != SceneManager.GetSceneByName("MainMenu_Scene").buildIndex)
+        {
+            levelIndex++;
+            levelIndex = (levelIndex >= SceneManager.sceneCountInBuildSettings) ? 1 : levelIndex;
+            SceneManager.LoadScene(levelIndex, LoadSceneMode.Single);
+        }
     }
 }
