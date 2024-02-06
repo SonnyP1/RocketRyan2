3018116354c7a7cf4d3fece3a61dd5e59e0019da
Jacob Hernandez <hernandez.jacob68@yahoo.com>
2024-02-05 18:53:36 -0600

    Whisp
    added whisp level and lvl6-7. new PE for when enemies spawn. made a self destruct script for stray bullets

---
+++ Assets/_Arcade/Enemy/Bosses/Prefabs/Whisp/Timer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float countDownTime = 60.0f;
+    private float currentTime;
+
+    void Start()
+    {
+        currentTime = countDownTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log("Timer = 0");
+        }
+    }
+}
